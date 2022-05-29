a9f6fe55f55bcafc4ab03c3c2ba2e5e87d4e5b8c
AsifSyeed <asifsyeed666@gmail.com>
2022-05-29 21:06:49 +0600

    Handling tap on Upcoming ViewController

--- CatchStream/Controllers/Core/UpcomingViewController.swift
+++ CatchStream/Controllers/Core/UpcomingViewController.swift
@@ -26,6 +26,7 @@ class UpcomingViewController: UIViewController {
         
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.tintColor = .white
         
         view.addSubview(upcomingTable)
         upcomingTable.delegate = self
@@ -72,5 +73,29 @@ extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let title = titles[indexPath.row]
+        
+        guard let titleName = title.original_title ?? title.original_name else {
+            return
+        }
+        
+        APICaller.shared.getMovie(with: titleName) { [weak self] result in
+            switch result {
+            case .success(let videoElement):
+                DispatchQueue.main.async {
+                    let vc = TitlePreviewViewController()
+                    vc.configure(with: TitlePreviewViewModel(title: titleName, youtubeView: videoElement, titleOverview: title.overview ?? ""))
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                    
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
