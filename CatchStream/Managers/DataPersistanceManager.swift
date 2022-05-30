fdd1ca2f4442e1a262f75b88939be4983e804f2c
AsifSyeed <asifsyeed666@gmail.com>
2022-05-30 12:44:27 +0600

    Implementing save and delete function for Downloads View Controller

--- CatchStream/Managers/DataPersistanceManager.swift
+++ CatchStream/Managers/DataPersistanceManager.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 import UIKit
+import CoreData
 
 class DataPersistanceManager {
     
     enum DatabaseError: Error {
         case failedToSaveData
+        case failedToFetchData
+        case failedToDeleteData
     }
     
     static let shared = DataPersistanceManager()
@@ -41,6 +44,46 @@ class DataPersistanceManager {
             completion(.success(()))
         } catch {
             completion(.failure(DatabaseError.failedToSaveData))
+        }
+    }
+    
+    func fetchingTitlesFromDatabase (completion: @escaping (Result<[TitleItem], Error>) -> Void) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request: NSFetchRequest<TitleItem>
+        
+        request = TitleItem.fetchRequest()
+        
+        do {
+            
+            let titles = try context.fetch(request)
+            completion(.success(titles))
+            
+        } catch {
+            completion(.failure(DatabaseError.failedToFetchData))
+        }
+    }
+    
+    func deleteTitleWith(model: TitleItem, completion: @escaping (Result<Void, Error>) -> Void) {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        context.delete(model)
+        
+        do {
+            try context.save()
+            completion(.success(()))
+        } catch {
+            completion(.failure(DatabaseError.failedToDeleteData))
         }
     }
 }
