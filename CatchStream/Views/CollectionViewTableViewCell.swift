b415d695dbf3e6f8ef8e35633fc23c828f36fd27
AsifSyeed <asifsyeed666@gmail.com>
2022-05-30 09:41:28 +0600

    Handling tapping action

--- CatchStream/Views/CollectionViewTableViewCell.swift
+++ CatchStream/Views/CollectionViewTableViewCell.swift
@@ -54,6 +54,10 @@ class CollectionViewTableViewCell: UITableViewCell {
             self?.collectionView.reloadData()
         }
     }
+    
+    private func downloadTitleAt(indexPath: IndexPath) {
+        print("Downloading \(String(describing: titles[indexPath.row].original_title))")
+    }
 }
 
 extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource{
@@ -106,5 +110,30 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        
+        let config = UIContextMenuConfiguration(
+            identifier: nil,
+            previewProvider: nil) { [weak self] _ in
+                let downloadAction = UIAction(
+                    title: "Download",
+                    subtitle: nil,
+                    image: nil,
+                    identifier: nil,
+                    discoverabilityTitle: nil,
+                    state: .off) { _ in
+                        self?.downloadTitleAt(indexPath: indexPath)
+                    }
+                return UIMenu(
+                    title: "",
+                    image: nil,
+                    identifier: nil,
+                    options: .displayInline,
+                    children: [downloadAction])
+            }
+        
+        return config
     }
 }
